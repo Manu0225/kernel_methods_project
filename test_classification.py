23fc3f6d9eefeea332921dd70fc2cce6983559a9
Waïss Azizian <waiss.azizian@ens.fr>
2021-02-18 14:11:52 +0100

    Add tests
    * Add default arguments
    
    * Add tests
    
    * Update README.md

---
+++ test_classification.py
@@ -0,0 +1,46 @@
+import pytest
+import numpy as np
+gen = np.random.default_rng()
+
+import methods
+import kernels
+
+n = 100
+d = 5
+
+@pytest.fixture
+def x():
+    x = gen.random(size=(n, d)) + 1
+    x[:n//2, :] *= -1
+    assert x.shape == (n, d)
+    return x
+
+@pytest.fixture
+def y():
+    y = np.ones((n, 1))
+    y[:n//2] = -1
+    assert y.shape == (n, 1)
+    return y
+
+x_train, x_test = x, x
+y_train, y_test = y, y
+
+TEST_METHODS = [
+            methods.KernelRidgeRegression,
+            methods.KernelLogisticRegression,
+        ]
+
+TEST_KERNELS = [
+            kernels.Linear,
+            kernels.Gaussian,
+        ]
+
+@pytest.mark.parametrize('method', TEST_METHODS)
+@pytest.mark.parametrize('kernel', TEST_KERNELS)
+def test_methods(method, kernel, x_train, y_train, x_test, y_test):
+    meth = method(kernel())
+    meth.learn(x_train, y_train)
+    y_est = meth.predict(x_train)
+    np.testing.assert_equal(y_est, y_train)
+    y_est = meth.predict(x_test)
+    np.testing.assert_equal(y_est, y_test)
