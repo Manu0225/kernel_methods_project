b8f5592e27f30890f131563a4bb84e2e5c7d6f71
Waiss Azizian <waiss.azizian@ens.fr>
2021-03-03 19:52:23 +0100

    Add spectrum kernel

--- kernels.py
+++ kernels.py
@@ -1,12 +1,23 @@
 from abc import ABC, abstractmethod
 import numpy as np
 from scipy.spatial.distance import cdist
+import scipy
+import scipy.sparse
 
 # X and X_prime are (n, d) and (m, d) numpy vectors resp.
 # Y is a (n,1) numpy vectors
 
 
 class Kernel(ABC):
+	@abstractmethod
+	def fit(self, X):
+		pass
+
+	@abstractmethod
+	def make_rkhs_func(self, alpha):
+		pass
+
+class NaiveKernel(ABC):
 	@abstractmethod
 	def K(self, X, X_prime):
 		pass
@@ -18,14 +29,14 @@ class Kernel(ABC):
 	def make_rkhs_func(self, alpha):
 		return lambda Xprime : self.K(Xprime, self.X) @ alpha
 
-class Linear(Kernel):
+class Linear(NaiveKernel):
 	def __init__(self):
 		super(Linear, self).__init__()
 
 	def K(self, X, X_prime):
 		return X @ X_prime.T
 
-class Gaussian(Kernel):
+class Gaussian(NaiveKernel):
 	def __init__(self, alpha=0.1):
 		super(Gaussian, self).__init__()
 		self.alpha = alpha
@@ -33,3 +44,40 @@ class Gaussian(Kernel):
 	def K(self, X, X_prime):
 		C = cdist(X, X_prime, 'euclidean') ** 2
 		return np.exp(- self.alpha / 2 * C)
+
+class FeaturesKernel(Kernel):
+	@abstractmethod
+	def features(self, X):
+		pass
+
+	def fit(self, X):
+		self.feats = self.features(X)
+		return (self.feats @ self.feats.T).todense().A
+
+	def make_rkhs_func(self, alpha):
+		w = self.feats.T.dot(alpha)
+		return lambda Xprime : self.features(Xprime).dot(w)
+
+class SpectrumKernel(FeaturesKernel):
+	def __init__(self, k):
+		self.k = k
+
+	def from_decomposition(self, decomp, basis):
+		n, _ = decomp.shape
+		assert decomp.shape == (n, self.k)
+
+		return decomp.dot(basis ** np.arange(self.k))
+
+	def features(self, X):
+		a = np.max(X) + 1
+		n, l = X.shape
+
+		phi = scipy.sparse.lil_matrix((n, a**self.k), dtype=np.uint)
+		indices = np.arange(n)
+		for i in range(self.k-1, l):
+			to_mod_indices = self.from_decomposition(X[:, i-(self.k-1):i+1], a)
+			for j, index in enumerate(to_mod_indices):
+				phi[j, index] += 1
+		return phi.tocsr()
+		
+
--- test_classification.py
+++ test_classification.py
@@ -8,7 +8,7 @@ import kernels
 
 n = 2000
 d = 100
-
+l = 106
 
 @pytest.fixture
 def x():
@@ -25,9 +25,16 @@ def y():
 	assert y.shape == (n, 1)
 	return y
 
+@pytest.fixture
+def seq():
+	seq = gen.choice([0,1],size=(n, l))
+	seq[n//2:, :] += 1
+	assert seq.shape == (n, l)
+	return seq
 
 x_train, x_test = x, x
 y_train, y_test = y, y
+seq_train, seq_test = seq, seq
 
 TEST_METHODS = [
 	methods.KernelRidgeRegression,
@@ -35,17 +42,31 @@ TEST_METHODS = [
 ]
 
 TEST_KERNELS = [
-	kernels.Linear,
-	kernels.Gaussian,
+	kernels.Linear(),
+	kernels.Gaussian(0.1),
 ]
 
+TEST_SEQ_KERNELS = [
+	kernels.SpectrumKernel(1),
+	kernels.SpectrumKernel(3),
+]
 
 @pytest.mark.parametrize('method', TEST_METHODS)
 @pytest.mark.parametrize('kernel', TEST_KERNELS)
 def test_methods(method, kernel, x_train, y_train, x_test, y_test):
-	meth = method(kernel())
+	meth = method(kernel)
 	meth.learn(x_train, y_train)
 	y_est = meth.predict(x_train)
 	np.testing.assert_equal(y_est, y_train)
 	y_est = meth.predict(x_test)
+	np.testing.assert_equal(y_est, y_test)
+
+@pytest.mark.parametrize('method', TEST_METHODS)
+@pytest.mark.parametrize('kernel', TEST_SEQ_KERNELS)
+def test_seq_methods(method, kernel, seq_train, y_train, seq_test, y_test):
+	meth = method(kernel)
+	meth.learn(seq_train, y_train)
+	y_est = meth.predict(seq_train)
+	np.testing.assert_equal(y_est, y_train)
+	y_est = meth.predict(seq_test)
 	np.testing.assert_equal(y_est, y_test)
