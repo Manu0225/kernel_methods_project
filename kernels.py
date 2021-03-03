f33220e6f5387d3616388bd30efe86c6ce746650
Manu0225 <manu.manu34@hotmail.fr>
2021-03-03 20:00:27 +0100

    Update kernels.py

--- kernels.py
+++ kernels.py
@@ -4,6 +4,7 @@ from scipy.spatial.distance import cdist
 import scipy
 import scipy.sparse
 
+
 # X and X_prime are (n, d) and (m, d) numpy vectors resp.
 # Y is a (n,1) numpy vectors
 
@@ -17,8 +18,12 @@ class Kernel(ABC):
 	def make_rkhs_func(self, alpha):
 		pass
 
+
 class NaiveKernel(ABC):
 	@abstractmethod
+	def __init__(self):
+		self.X = None
+
 	def K(self, X, X_prime):
 		pass
 
@@ -27,7 +32,8 @@ class NaiveKernel(ABC):
 		return self.K(X, X)
 
 	def make_rkhs_func(self, alpha):
-		return lambda Xprime : self.K(Xprime, self.X) @ alpha
+		return lambda Xprime: self.K(Xprime, self.X) @ alpha
+
 
 class Linear(NaiveKernel):
 	def __init__(self):
@@ -35,6 +41,7 @@ class Linear(NaiveKernel):
 
 	def K(self, X, X_prime):
 		return X @ X_prime.T
+
 
 class Gaussian(NaiveKernel):
 	def __init__(self, alpha=0.1):
@@ -45,7 +52,11 @@ class Gaussian(NaiveKernel):
 		C = cdist(X, X_prime, 'euclidean') ** 2
 		return np.exp(- self.alpha / 2 * C)
 
+
 class FeaturesKernel(Kernel):
+	def __init__(self):
+		self.feats = None
+
 	@abstractmethod
 	def features(self, X):
 		pass
@@ -56,10 +67,12 @@ class FeaturesKernel(Kernel):
 
 	def make_rkhs_func(self, alpha):
 		w = self.feats.T.dot(alpha)
-		return lambda Xprime : self.features(Xprime).dot(w)
+		return lambda Xprime: self.features(Xprime).dot(w)
+
 
 class SpectrumKernel(FeaturesKernel):
 	def __init__(self, k):
+		super(SpectrumKernel, self).__init__()
 		self.k = k
 
 	def from_decomposition(self, decomp, basis):
@@ -70,14 +83,21 @@ class SpectrumKernel(FeaturesKernel):
 
 	def features(self, X):
 		a = np.max(X) + 1
-		n, l = X.shape
+		n, length = X.shape
 
-		phi = scipy.sparse.lil_matrix((n, a**self.k), dtype=np.uint)
-		indices = np.arange(n)
-		for i in range(self.k-1, l):
-			to_mod_indices = self.from_decomposition(X[:, i-(self.k-1):i+1], a)
+		phi = scipy.sparse.lil_matrix((n, a ** self.k), dtype=np.uint)
+		for i in range(self.k - 1, length):
+			to_mod_indices = self.from_decomposition(X[:, i - (self.k - 1):i + 1], a)
 			for j, index in enumerate(to_mod_indices):
 				phi[j, index] += 1
 		return phi.tocsr()
-		
 
+
+class Polynomial(NaiveKernel):
+	def __init__(self, degree):
+		super(Polynomial, self).__init__()
+		assert (isinstance(degree, int))
+		self.degree = degree
+
+	def K(self, X, X_prime):
+		return (X @ X_prime.T) ** self.degree
