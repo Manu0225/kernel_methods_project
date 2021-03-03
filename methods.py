49ad6b95b69aa2ac3cacb1e8b52c9576e556d991
Manu0225 <manu.manu34@hotmail.fr>
2021-03-03 17:11:25 +0100

    changed spaces to tabs for indent

--- methods.py
+++ methods.py
@@ -1,124 +1,134 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
+
 class Method(ABC):
-    def __init__(self):
-        self.kernel = None
+	def __init__(self):
+		self.kernel = None
 
-    @abstractmethod
-    def learn(self, X, Y):
-        self.X = None
-        self.alpha = None
-        pass
+	@abstractmethod
+	def learn(self, X, Y):
+		self.X = None
+		self.alpha = None
+		pass
 
-    def predict(self, X_prime):
-        K = self.kernel.K(X_prime, self.X)
+	def predict(self, X_prime):
+		K = self.kernel.K(X_prime, self.X)
 
-        n, _ = self.alpha.shape
-        m, _ = K.shape
-        assert K.shape == (m, n)
+		n, _ = self.alpha.shape
+		m, _ = K.shape
+		assert K.shape == (m, n)
 
-        f = K @ self.alpha
-        assert f.shape == (m, 1)
+		f = K @ self.alpha
+		assert f.shape == (m, 1)
 
-        h = np.sign(f)
-        h[h==0] = 1
+		h = np.sign(f)
+		h[h == 0] = 1
 
-        return h
+		return h
+
 
 class KernelRidgeRegression(Method):
-    def __init__(self, kernel, reg_val=0.1):
-        self.kernel = kernel
-        self.reg_val = reg_val
+	def __init__(self, kernel, reg_val=0.1):
+		self.kernel = kernel
+		self.reg_val = reg_val
 
-    def learn(self, X, Y):
-        self.X = X
-        K = self.kernel.K(X, X)
-        n, _ = K.shape
-        assert Y.shape == (n, 1)
+	def learn(self, X, Y):
+		self.X = X
+		K = self.kernel.K(X, X)
+		n, _ = K.shape
+		assert Y.shape == (n, 1)
 
-        self.alpha = np.linalg.solve(K + self.reg_val * n * np.identity(n), Y)
-        assert self.alpha.shape == (n, 1)
+		self.alpha = np.linalg.solve(K + self.reg_val * n * np.identity(n), Y)
+		assert self.alpha.shape == (n, 1)
+
 
 def line_search(f, f_x, nt_decrement, x, delta_x, alpha, beta):
-    t = 1
-    scalar_product = alpha * (-nt_decrement)
-    assert(scalar_product < 0)
-    in_dom, val = f(x + t * delta_x)
-    while (not in_dom) or val > f_x + t * scalar_product:
-        t = beta * t
-        in_dom, val = f(x + t * delta_x)
-    return t
+	t = 1
+	scalar_product = alpha * (-nt_decrement)
+	assert (scalar_product < 0)
+	in_dom, val = f(x + t * delta_x)
+	while (not in_dom) or val > f_x + t * scalar_product:
+		t = beta * t
+		in_dom, val = f(x + t * delta_x)
+	return t
+
 
 def newton_step(grad_f_x, hessian_f_x):
-    nt_step = -np.linalg.lstsq(hessian_f_x, grad_f_x, rcond=None)[0]
-    nt_decrement = - np.dot(grad_f_x.T, nt_step)
-    return nt_step, nt_decrement
+	nt_step = -np.linalg.lstsq(hessian_f_x, grad_f_x, rcond=None)[0]
+	nt_decrement = - np.dot(grad_f_x.T, nt_step)
+	return nt_step, nt_decrement
+
 
 def newton_method(f, f_deriv, x_0, eps=1e-7, alpha=0.1, beta=0.5):
-    x = x_0
-    n = 0
-    while True:
-        f_x, grad_f_x, hessian_f_x = f_deriv(x)
-        nt_step, nt_decrement = newton_step(grad_f_x, hessian_f_x)
-        if nt_decrement / 2 <= eps:
-            break
-        t = line_search(f, f_x, nt_decrement, x, nt_step, alpha, beta)
-        x = x + t * nt_step
-        n += 1
-    return x
+	x = x_0
+	n = 0
+	while True:
+		f_x, grad_f_x, hessian_f_x = f_deriv(x)
+		nt_step, nt_decrement = newton_step(grad_f_x, hessian_f_x)
+		if nt_decrement / 2 <= eps:
+			break
+		t = line_search(f, f_x, nt_decrement, x, nt_step, alpha, beta)
+		x = x + t * nt_step
+		n += 1
+	return x
+
 
 def logistic(u):
-    """ logistic function """
-    exp_u_pos_part = np.exp(-np.clip(u, 0, None))
-    exp_u_neg_part = np.exp( np.clip(u, None, 0))
-    return - np.clip(u, None, 0) + np.log1p(exp_u_neg_part + exp_u_pos_part - 1)
+	""" logistic function """
+	exp_u_pos_part = np.exp(-np.clip(u, 0, None))
+	exp_u_neg_part = np.exp(np.clip(u, None, 0))
+	return - np.clip(u, None, 0) + np.log1p(exp_u_neg_part + exp_u_pos_part - 1)
+
 
 def sigmoid(u):
-    """ sigmoid function """
-    exp_u_pos_part = np.exp(-np.clip(u, 0, None))
-    exp_u_neg_part = np.exp( np.clip(u, None, 0))
-    return exp_u_neg_part/(exp_u_neg_part + exp_u_pos_part)
+	""" sigmoid function """
+	exp_u_pos_part = np.exp(-np.clip(u, 0, None))
+	exp_u_neg_part = np.exp(np.clip(u, None, 0))
+	return exp_u_neg_part / (exp_u_neg_part + exp_u_pos_part)
+
 
 def make_kernel_logistic_regression_funcs(K, Y, reg_val):
-    n, _ = Y.shape
-    assert Y.shape == (n, 1)
-    assert K.shape == (n, n)
-    
-    def loss(alpha):
-        assert alpha.shape == (n, 1)
-        K_alpha = K @ alpha
-        return True, np.sum(logistic(Y * K_alpha))/n + (reg_val/2) * np.dot(alpha.T, K_alpha)
-        
-    def oracle(alpha):
-        assert alpha.shape == (n, 1)
-        K_alpha = K @ alpha
-        value = np.sum(logistic(Y * K_alpha))/n + (reg_val/2) * np.dot(alpha.T, K_alpha)
+	n, _ = Y.shape
+	assert Y.shape == (n, 1)
+	assert K.shape == (n, n)
 
-        P = -sigmoid(- Y * K_alpha)
-        grad = (1/n) * K @ (P * Y) + reg_val * K_alpha
+	def loss(alpha):
+		assert alpha.shape == (n, 1)
+		K_alpha = K @ alpha
+		return True, np.sum(logistic(Y * K_alpha)) / n + (reg_val / 2) * np.dot(alpha.T, K_alpha)
 
-        W = sigmoid(Y * K_alpha) * sigmoid(- Y * K_alpha)
-        hessian = (1/n) * K @ (W * K) + reg_val * K
-        return value, grad, hessian
+	def oracle(alpha):
+		assert alpha.shape == (n, 1)
+		K_alpha = K @ alpha
+		value = np.sum(logistic(Y * K_alpha)) / n + (reg_val / 2) * np.dot(alpha.T, K_alpha)
 
-    return loss, oracle
+		P = -sigmoid(- Y * K_alpha)
+		grad = (1 / n) * K @ (P * Y) + reg_val * K_alpha
+
+		W = sigmoid(Y * K_alpha) * sigmoid(- Y * K_alpha)
+		hessian = (1 / n) * K @ (W * K) + reg_val * K
+		return value, grad, hessian
+
+	return loss, oracle
+
 
 class KernelLogisticRegression(Method):
-    def __init__(self, kernel, reg_val=0.1):
-        self.kernel = kernel
-        self.reg_val = reg_val
+	def __init__(self, kernel, reg_val=0.1):
+		self.kernel = kernel
+		self.reg_val = reg_val
 
-    def learn(self, X, Y, tol=1e-6):
-        self.X = X
-        K = self.kernel.K(X, X)
-        n, _ = K.shape
-        assert Y.shape == (n, 1)
-        
-        loss, oracle = make_kernel_logistic_regression_funcs(K, Y, self.reg_val)
-        alpha_0 = np.ones((n, 1))
-        alpha = newton_method(loss, oracle, alpha_0, eps=tol)
-        self.alpha = alpha
+	def learn(self, X, Y, tol=1e-6):
+		self.X = X
+		K = self.kernel.K(X, X)
+		n, _ = K.shape
+		assert Y.shape == (n, 1)
+
+		loss, oracle = make_kernel_logistic_regression_funcs(K, Y, self.reg_val)
+		alpha_0 = np.ones((n, 1))
+		alpha = newton_method(loss, oracle, alpha_0, eps=tol)
+		self.alpha = alpha
+
 
 def accuracy(y_true, y_pred):
-    return np.sum(y_true == y_pred)/len(y_true)
+	return np.sum(y_true == y_pred) / len(y_true)
--- test_classification.py
+++ test_classification.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+
 gen = np.random
 
 import methods
@@ -8,39 +9,43 @@ import kernels
 n = 2000
 d = 100
 
+
 @pytest.fixture
 def x():
-    x = gen.random(size=(n, d)) + 1
-    x[:n//2, :] *= -1
-    assert x.shape == (n, d)
-    return x
+	x = gen.random(size=(n, d)) + 1
+	x[:n // 2, :] *= -1
+	assert x.shape == (n, d)
+	return x
+
 
 @pytest.fixture
 def y():
-    y = np.ones((n, 1))
-    y[:n//2] = -1
-    assert y.shape == (n, 1)
-    return y
+	y = np.ones((n, 1))
+	y[:n // 2] = -1
+	assert y.shape == (n, 1)
+	return y
+
 
 x_train, x_test = x, x
 y_train, y_test = y, y
 
 TEST_METHODS = [
-            methods.KernelRidgeRegression,
-            methods.KernelLogisticRegression,
-        ]
+	methods.KernelRidgeRegression,
+	methods.KernelLogisticRegression,
+]
 
 TEST_KERNELS = [
-            kernels.Linear,
-            kernels.Gaussian,
-        ]
+	kernels.Linear,
+	kernels.Gaussian,
+]
+
 
 @pytest.mark.parametrize('method', TEST_METHODS)
 @pytest.mark.parametrize('kernel', TEST_KERNELS)
 def test_methods(method, kernel, x_train, y_train, x_test, y_test):
-    meth = method(kernel())
-    meth.learn(x_train, y_train)
-    y_est = meth.predict(x_train)
-    np.testing.assert_equal(y_est, y_train)
-    y_est = meth.predict(x_test)
-    np.testing.assert_equal(y_est, y_test)
+	meth = method(kernel())
+	meth.learn(x_train, y_train)
+	y_est = meth.predict(x_train)
+	np.testing.assert_equal(y_est, y_train)
+	y_est = meth.predict(x_test)
+	np.testing.assert_equal(y_est, y_test)
