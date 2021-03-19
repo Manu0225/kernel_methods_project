add better caching and normalisation, and tried to fix the Sum Kernel

main.py
@@ -3,36 +3,29 @@ import numpy as np
 import read_write
 import kernels
 import methods  # .methods as methods
-from multiprocessing import Pool
 
 
-def generate_kernel_matrices(X_train, method_class, kernel_class, ls_params):
+# from multiprocessing import Pool
+
+
+def generate_kernel_matrices(X_train, kernel_class, ls_params):
 	ls_kernels = [kernel_class(*param) for param in ls_params]
 
-	ls_K = []
-	ls_phi = []
-
-	for i in range(len(ls_params)):
-		method = method_class(ls_kernels[i])
-		K, phi = method.kernel.fit(X_train)
-		ls_K.append(K)
-		ls_phi.append(phi)
-
-	ls_K = np.array(ls_K)
-	ls_phi = np.array(ls_phi)
-	return ls_K, ls_phi
+	for kernel in ls_kernels:
+		K, phi = kernel.fit(X_train)
+		yield K, phi
 
 
 def main_sum():
 	ls_kernel = [
-		kernels.SpectrumKernel(15),
-		kernels.SpectrumKernel(6),
-		kernels.SpectrumKernel(6)
+		kernels.SpectrumKernel(8, True),
+		kernels.SpectrumKernel(6, True),
+		kernels.SpectrumKernel(6, True)
 	]
 	ls_kernel_prime = [
-		kernels.Gaussian(1),
-		kernels.Gaussian(1),
-		kernels.Gaussian(.01)
+		kernels.Gaussian(.1, True),
+		kernels.Gaussian(.1, True),
+		kernels.Gaussian(.1, True)
 	]
 	# ls_methods = [
 	# methods.SVM(kernel_0, reg_val=.1),
@@ -44,7 +37,7 @@ def main_sum():
 			  f"i={i}")
 		# X = read_write.read_X100(f"data/Xtr{i}_mat100.csv")
 		X = read_write.read(f"data/Xtr{i}.csv")
-		l = X.shape[1]
+		length = X.shape[1]
 
 		X_cat = np.concatenate((X, X), axis=-1)
 
@@ -53,15 +46,14 @@ def main_sum():
 
 		y = read_write.read_labels(f"data/Ytr{i}.csv")
 
-		X_train, y_train, X_val, y_val = split(X_cat, y, proportion_train=.8)
 		# k,m,A
 
 		kernel_class = kernels.SumKernel
-		method_class = methods.SVM
-		params_kernel = [(ls_kernel[i], ls_kernel_prime[i], l, l)]
+		method_class = methods.KernelRidgeRegression
+		params_kernel = [(ls_kernel[i], ls_kernel_prime[i], length, length, False)]
 		# [(k, max(floor(k / 10), 1), 4) for k in range(6, 18, 3)]  # 10. **
-		reg_vals = 10. ** np.arange(-2, 3, 1 / 2)
-		res = validation(X_train, y_train, X_val, y_val, kernel_class, method_class, params_kernel, reg_vals)
+		reg_vals = 10. ** np.arange(-2, 3, 1 / 4)
+		validation(X_cat, y, kernel_class, method_class, params_kernel, reg_vals)
 
 
 def main_poly():
@@ -73,25 +65,18 @@ def main_poly():
 	for i in range(3):
 		print("##################",
 			  f"i={i}")
-		# X = read_write.read_X100(f"data/Xtr{i}_mat100.csv")
 		X = read_write.read(f"data/Xtr{i}.csv")
 
-		# X_test = read_write.read_X100(f"data/Xte{i}_mat100.csv")
-		# X_test = read_write.read(f"data/Xte{i}.csv")
-
 		y = read_write.read_labels(f"data/Ytr{i}.csv")
-
-		X_train, y_train, X_val, y_val = split(X, y, proportion_train=.8)
-		# k,m,A
 
 		kernel_class = kernels.PolyKernel
 		method_class = methods.KernelLogisticRegression
 		print(kernel_class, method_class)
-		params_kernel = [(ls_kernel[i], deg) for deg in range(2, 5)]
+		params_kernel = [(ls_kernel[i], deg, True) for deg in range(2, 6)]
 		# [(k, max(floor(k / 10), 1), 4) for k in range(6, 18, 3)]  # 10. **
 		reg_vals = 10. ** np.arange(-3, 4, 1)
 
-		res = validation(X_train, y_train, X_val, y_val, kernel_class, method_class, params_kernel, reg_vals)
+		validation(X, y, kernel_class, method_class, params_kernel, reg_vals)
 
 
 def main_val():
@@ -106,18 +91,17 @@ def main_val():
 
 		y = read_write.read_labels(f"data/Ytr{i}.csv")
 
-		X_train, y_train, X_val, y_val = split(X, y, proportion_train=.8)
 		# k,m,A
-		kernel_class = kernels.MismatchKernel
-		method_class = methods.SVM
-		params_kernel = [(i, max(floor(i / 10), 1), 4) for i in range(3, 16, 2)]  # Mismatch
-		# params_kernel = [(10.**i,) for i in range(-2, 2, 1)] # gaussian
-		reg_vals = 10. ** np.arange(-2, 3, 1.5)
+		kernel_class = kernels.Gaussian
 
-		ls_K, ls_phi = generate_kernel_matrices(X_train, method_class, kernel_class, params_kernel)
+		method_class = methods.KernelRidgeRegression
+		# params_kernel = [(i, True) for i in range(4, 16, 2)]  # Spectrum
+		# params_kernel = [(i, 0, 4, True) for i in range(4, 16, 2)]  # Mismatch #max(floor(i / 5)
+		params_kernel = [(10. ** i, True) for i in range(-3, 3, 1)]  # gaussian
+		reg_vals = 10. ** np.arange(-2, 3, 1)
 
-		res = validation(X_train, y_train, X_val, y_val,
-						 kernel_class, ls_K, ls_phi, method_class, params_kernel, reg_vals)
+		validation(X, y,
+				   kernel_class, method_class, params_kernel, reg_vals)
 
 
 # print(res)
@@ -125,37 +109,49 @@ def main_val():
 
 def main_rendu():
 	ls_kernel = [
-		kernels.MismatchKernel(12, 1, 4),
-		kernels.MismatchKernel(12, 1, 4),
-		kernels.MismatchKernel(9, 1, 4)
+		kernels.SpectrumKernel(8, True),
+		kernels.SpectrumKernel(6, True),
+		kernels.SpectrumKernel(6, True)
 	]
 	ls_kernel_prime = [
-		kernels.Gaussian(1),
-		kernels.Gaussian(1),
-		kernels.Gaussian(.01)
+		kernels.Gaussian(.1, True),
+		kernels.Gaussian(.1, True),
+		kernels.Gaussian(.1, True)
 	]
-	ls_reg_val = [1, 10, 1]
+	# ls_kernel = [#manque des true
+	# 	kernels.MismatchKernel(10, 1, 4),
+	# 	kernels.MismatchKernel(9, 1, 4),
+	# 	kernels.MismatchKernel(8, 1, 4)
+	# ]
+	ls_reg_val = [0.03162277660168379, 0.03162277660168379, 0.01]
 	ls_methods = [
-		methods.SVM(ls_kernel[i],
-					reg_val=ls_reg_val[i])
+		methods.KernelRidgeRegression(
+			kernels.SumKernel(ls_kernel[i], ls_kernel_prime[i], 101, 101, False),
+			reg_val=ls_reg_val[i])
 		for i in range(3)
+		# methods.SVM(ls_kernel[1],
+		# 			reg_val=ls_reg_val[1]),
+		# methods.KernelRidgeRegression(ls_kernel[2],
+		# 							  reg_val=ls_reg_val[2]),
 	]
 	for i in range(3):
 		print("##################",
 			  f"i={i}")
 		# X = read_write.read_X100(f"data/Xtr{i}_mat100.csv")
 		X = read_write.read(f"data/Xtr{i}.csv")
+		X_cat = np.concatenate((X, X), axis=-1)
 
 		# X_test = read_write.read_X100(f"data/Xte{i}_mat100.csv")
 		X_test = read_write.read(f"data/Xte{i}.csv")
+		X_test_cat = np.concatenate((X_test, X_test), axis=-1)
 
 		y = read_write.read_labels(f"data/Ytr{i}.csv")
 		# X_cat = np.concatenate((X, X), axis=-1)
 		# X_test_cat = np.concatenate((X_test, X_test), axis=-1)
-		ls_methods[i].learn(X, y, normalize=False)
-		y_pred = ls_methods[i].predict(X)
+		ls_methods[i].learn(X_cat, y)
+		y_pred = ls_methods[i].predict(X_cat)
 		print(methods.accuracy(y, y_pred))
-		y_test = ls_methods[i].predict(X_test)
+		y_test = ls_methods[i].predict(X_test_cat)
 
 		read_write.write(y_test,
 						 "predictions/Yte.csv",
@@ -165,7 +161,7 @@ def main_rendu():
 
 
 def main():
-	main_val()
+	main_rendu()
 	return
 
 
@@ -191,7 +187,22 @@ def split(X, y, proportion_train):
 	n_train = floor(N * proportion_train)
 	X_train, y_train = X[sigma][:n_train], y[sigma][:n_train]
 	X_val, y_val = X[sigma][n_train:], y[sigma][n_train:]
-	return X_train, y_train, X_val, y_val
+	return X_train, y_train, X_val, y_val, sigma
+
+
+def extract_kernel(K, feats, sigma, n_train):
+	K_train = K[sigma, :][:, sigma][:n_train, :n_train]
+	K_val = K[sigma, :][:, sigma][n_train:, :n_train]
+	if feats is None:
+		feats_train, feats_val = None, None
+	elif isinstance(feats, np.ndarray):
+		feats_train = feats[sigma][:n_train]
+		feats_val = feats[sigma][n_train:]
+	else:
+		# feats is assumed to be an iterable of ndarrays
+		feats_train = [f[sigma][:n_train] if f is not None else None for f in feats]
+		feats_val = [f[sigma][n_train:] if f is not None else None for f in feats]
+	return K_train, feats_train, K_val, feats_val
 
 
 # def eval(X_train, y_train, X_val, y_val, param, reg_val, kernel_class, method_class):
@@ -204,25 +215,48 @@ def split(X, y, proportion_train):
 # 	return acc, param, reg_val
 
 
-def validation(X_train, y_train, X_val, y_val, kernel_class, ls_K, ls_phi,
-			   method_class, params_kernel, reg_vals):
-	best_acc = 0
-	best_param = None
-	best_reg_val = None
+def validation(X, y, kernel_class, method_class, params_kernel, reg_vals, n_iters=5):
+	# best_acc = 0
+	# best_param = None
+	# best_reg_val = None
+	generator_kernel_matrices = \
+		generate_kernel_matrices(X, kernel_class, ls_params=params_kernel)
+	for param, (K, phi) in zip(params_kernel, generator_kernel_matrices):
+		try:
+			ls_acc = np.zeros((n_iters, len(reg_vals)))
+			for it in range(n_iters):
+				X_train, y_train, X_val, y_val, sigma = split(X, y, proportion_train=.8)
+				n_train = len(X_train)
+				n_val = len(X_val)
+				K_train, feats_train, K_val, feats_val = extract_kernel(K, phi, sigma, n_train)
+				assert K_train.shape == (n_train, n_train)
+				assert K_val.shape == (n_val, n_train)
 
-	for i, param in enumerate(params_kernel):
-		for j, reg_val in enumerate(reg_vals):
-			kernel = kernel_class(*param)
-			method = method_class(kernel, reg_val=reg_val)
-			method.learn(X_train, y_train, K=ls_K[i], phi=ls_phi[i], normalize=True)
-			y_pred = method.predict(X_val)
-			acc = methods.accuracy(y_val, y_pred)
-			print(f"{acc}, {param}, {reg_val}")
-			if acc > best_acc:
-				best_acc = acc
-				best_param = param
-				best_reg_val = reg_val
-	return best_acc, best_param, best_reg_val
+				for j, reg_val in enumerate(reg_vals):
+					kernel = kernel_class(*param)
+					method = method_class(kernel, reg_val=reg_val)
+					method.learn(X_train, y_train, K=K_train, phi=feats_train)
+					y_pred = method.predict(X_val, K_prime=K_val, phi_prime=feats_val)
+					acc = methods.accuracy(y_val, y_pred)
+					ls_acc[it, j] = acc
+			avg_acc = np.mean(ls_acc, axis=0)
+			std_acc = np.std(ls_acc, axis=0)
+			for acc, reg_val, std in zip(avg_acc, reg_vals, std_acc):
+				print(f"{acc:.4f}, {param}, {reg_val}, {std:.3f}")
+		except Exception as e:
+			raise (e)
+			print("petit message pour nous")
+			print(type(e))
+			print(e)
+
+
+# if acc > best_acc:
+# 	best_acc = acc
+# 	best_param = param
+# 	best_reg_val = reg_val
+
+
+# return best_acc, best_param, best_reg_val
 
 
 if __name__ == '__main__':

methods/methods.py
@@ -11,18 +11,15 @@ class Method(ABC):
 	def _kernel_learn(self, K, Y):
 		pass
 
-	def learn(self, X, Y, K=None, phi=None, normalize=False):
+	def learn(self, X, Y, K=None, phi=None):
+		# j’ai l’impression que la normalisation prend un temps infini sur Mismatch…
 		K, _ = self.kernel.fit(X, K=K, phi=phi)
-		norm_matrix = np.diag(K).reshape(-1, 1) @ np.diag(K).reshape(1,-1) if normalize else 1  # np.amax(np.abs(K))
-		K = K / norm_matrix
+		# norm_matrix = np.diag(K).reshape(-1, 1) @ np.diag(K).reshape(1,-1) if normalize else 1  # np.amax(np.abs(K))
+		# K = K / norm_matrix
 		self.alpha = self._kernel_learn(K, Y)
-		# self.alpha /= norm_matrix
-		# QUE METTRE ICI ? np.diag(K) ?
 
-	# return K, phi, self.alpha
-
-	def predict(self, X_prime):
-		rkhs_func = self.kernel.make_rkhs_func(self.alpha)
+	def predict(self, X_prime, K_prime=None, phi_prime=None):
+		rkhs_func = self.kernel.make_rkhs_func(self.alpha, K_prime, phi_prime)
 		f = rkhs_func(X_prime)
 
 		h = np.sign(f)

test_classification.py
@@ -7,7 +7,7 @@ gen = np.random
 
 n = 1000
 d = 100
-l = 106
+length = 106
 A = 3
 
 
@@ -29,16 +29,18 @@ def y():
 
 @pytest.fixture
 def seq():
-	seq = gen.choice([0, 1], size=(n, l))
+	seq = gen.choice([0, 1], size=(n, length))
 	seq[n // 2:, :] += 1
-	assert seq.shape == (n, l)
+	assert seq.shape == (n, length)
 	return seq
+
 
 @pytest.fixture
 def simple_seq():
-	seq = np.zeros((10, l))
+	seq = np.zeros((10, length))
 	seq[10 // 2:, :] += 1
 	return seq
+
 
 x_train, x_test = x, x
 y_train, y_test = y, y
@@ -57,8 +59,8 @@ TEST_KERNELS = [
 ]
 
 TEST_SEQ_KERNELS = [
-	#kernels.SpectrumKernel(1),
-	#kernels.SpectrumKernel(2),
+	# kernels.SpectrumKernel(1),
+	# kernels.SpectrumKernel(2),
 	kernels.MismatchKernel(3, 1, A)
 ]
 
@@ -67,6 +69,7 @@ TEST_SEQ_KERNELS = [
 @pytest.mark.parametrize('kernel_with_reg', TEST_KERNELS)
 def test_methods(method, kernel_with_reg, x_train, y_train, x_test, y_test):
 	kernel, reg = kernel_with_reg
+	kernel.normalize = True
 	meth = method(kernel, reg_val=reg)
 	meth.learn(x_train, y_train)
 	y_est = meth.predict(x_train)
@@ -78,9 +81,10 @@ def test_methods(method, kernel_with_reg, x_train, y_train, x_test, y_test):
 @pytest.mark.parametrize('method', TEST_METHODS)
 @pytest.mark.parametrize('kernel', TEST_SEQ_KERNELS)
 def test_seq_methods(method, kernel, seq_train, y_train, seq_test, y_test):
-	K = kernel.fit(seq_train)
+	kernel.normalize = True
+	K, _ = kernel.fit(seq_train)
 	np.testing.assert_equal(K, K.T)
-	assert (np.all(np.linalg.eigvalsh(K) > 0)),f"Sp(K)={np.sort(np.real(np.linalg.eigvalsh(K)))}"
+	assert (np.all(np.round(np.linalg.eigvalsh(K), 10) >= 0)), f"Sp(K)={np.sort(np.real(np.linalg.eigvalsh(K)))}"
 	meth = method(kernel)
 	meth.learn(seq_train, y_train)
 	y_est = meth.predict(seq_train)
@@ -88,17 +92,14 @@ def test_seq_methods(method, kernel, seq_train, y_train, seq_test, y_test):
 	y_est = meth.predict(seq_test)
 	np.testing.assert_equal(y_est, y_test)
 
+
 def test_mismatch_kernel(simple_seq):
 	n = len(simple_seq)
 	kernel = kernels.MismatchKernel(3, 1, 2)
-	K = kernel.fit(simple_seq)
+	K, _ = kernel.fit(simple_seq)
 	print(K)
-	assert (K[:n//2, :n//2] >= 1).all()
-	assert (K[n//2:, n//2:] >= 1).all()
-	zeros = np.zeros((n//2, n//2))
-	np.testing.assert_equal(K[:n//2, n//2:], zeros)
-	np.testing.assert_equal(K[n//2:, :n//2], zeros)
-
-
-
-
+	assert (K[:n // 2, :n // 2] >= 1).all()
+	assert (K[n // 2:, n // 2:] >= 1).all()
+	zeros = np.zeros((n // 2, n // 2))
+	np.testing.assert_equal(K[:n // 2, n // 2:], zeros)
+	np.testing.assert_equal(K[n // 2:, :n // 2], zeros)